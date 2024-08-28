4715c332399e9b2079b650e589e92b6e36441525
Larissa Alves <alves04.larissa@gmail.com>
2024-08-28 19:17:36 -0300

    alteracoes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,8 @@
 	export let data: PageData;
 
 	console.log('TESTE DO FRONTEND', form?.teste[0]);
-	kjaskajsk;
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <form action="?/teste" method="post">
 	<button type="submit">Teste consulta</button>
