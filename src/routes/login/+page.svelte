07e4e442d910363ae9eda1cd13f5a7dcfeca9f02
Larissa Alves <alves04.larissa@gmail.com>
2024-09-13 18:19:49 -0300

    pagina de login mobile

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import loginPage from '$lib/img/loginPage.png';
+	import loginMobile from '$lib/img/loginMobile.png';
 	import { Button } from '$lib/components/ui/button';
 	import googleIcon from '$lib/img/googleIcon.svg';
 	import facebookIcon from '$lib/img/facebookIcon.svg';
@@ -7,24 +8,29 @@
 </script>
 
 <div class="relative flex h-full w-full justify-center md:justify-end">
-	<img class="absolute h-screen w-full blur-sm md:blur-none" src={loginPage} alt="" />
+	<img class=" absolute h-screen w-full md:hidden" src={loginMobile} alt="Mobile Background" />
+	<img class="absolute hidden h-screen w-full md:block" src={loginPage} alt="Desktop Background" />
+
 	<div
-		class="absolute flex h-screen w-full flex-col items-center justify-center gap-7 md:w-1/2 md:items-start"
+		class="absolute flex h-screen w-full flex-col items-center justify-between gap-7 py-11 md:w-1/2 md:items-start md:justify-center md:py-0"
 	>
-		<img class="w-32 md:hidden" src={logoCookiesMo} alt="" />
-		<div class="flex flex-col items-center md:items-start">
-			<h1 class=" text-2xl font-semibold">Login</h1>
-			<p class=" text-xs text-zinc-500">Acesse sua conta para continuar</p>
-		</div>
-		<div class="flex w-full flex-col items-center gap-4 md:w-2/3 md:flex-row md:items-start">
-			<Button class="h-11 w-3/4 gap-2 bg-white text-black hover:text-white">
-				<img class="w-8" src={googleIcon} alt="Google" />
-				Faça login com o Google</Button
-			>
-			<Button class="h-12 w-3/4 gap-2 bg-white text-black hover:text-white md:h-11">
-				<img class="w-8" src={facebookIcon} alt="Google" />
-				Faça login com o Google</Button
-			>
+		<img class="w-32 md:hidden" src={logoCookiesMo} alt="Logo Mobile" />
+
+		<div class="flex w-full flex-col items-center gap-6 md:items-start">
+			<div class="flex flex-col items-center md:items-start">
+				<h1 class="text-2xl font-semibold">Login</h1>
+				<p class="text-xs text-zinc-700">Acesse sua conta para continuar</p>
+			</div>
+			<div class="flex flex-col items-center gap-4 md:w-full md:flex-row">
+				<Button class="h-11 w-full gap-2 bg-white text-black hover:text-white md:w-2/6">
+					<img class="w-8" src={googleIcon} alt="Google" />
+					Faça login com o Google
+				</Button>
+				<Button class="h-12 gap-2 bg-white text-black hover:text-white md:h-11 md:w-2/6">
+					<img class="w-8" src={facebookIcon} alt="Facebook" />
+					Faça login com o Facebook
+				</Button>
+			</div>
 		</div>
 	</div>
 </div>
