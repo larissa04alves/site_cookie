e010ed2d167a4204b5c38eb976188996d2d67c8b
Othavio Quiliao <othavioaqa@gmail.com>
2024-10-01 16:29:37 -0300

    Update do home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,9 @@
 </script>
 
 <div
-	class="flex h-screen w-full flex-col items-center justify-center font-cormorant text-4xl font-bold text-brownCrayola"
+	class="flex h-screen w-full flex-col items-center justify-center gap-10 font-cormorant text-4xl font-bold text-brownCrayola"
 >
-	<h1>Va para:</h1>
+	<h1>Teste as rotas:</h1>
 	<a href="/login" class="hover:underline">/login</a>
+	<a href="/admin/produtos" class="hover:underline">/admin/produtos</a>
 </div>
